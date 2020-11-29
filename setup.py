12ae3327fdcf2e84e32c385e373e277f0d68fb7a
loxygenK <loxygen.k@gmail.com>
2020-11-29 21:18:58 +0900

    🚀 Write setup.py detail

--- setup.py
+++ setup.py
@@ -1,13 +1,41 @@
 from setuptools import setup
 
+
+def load_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def load_readme():
+    with open("README.rst") as f:
+        return f.read()
+
+version = "0.1.0"
+
 setup(
     name="arduino-oxidizer",
-    version="0.1.0",
+    version=version,
+    author="loxygen",
+    author_email="me@loxygen.dev",
+    maintainer="loxygen",
+    maintainer_email="me@loxygen.dev",
+    description="A python tool to build Rust project for Arduino and write it.",
+    long_description=load_readme(),
     packages=["src"],
-    install_requires=["pyserial"],
+    install_requires=load_requirements(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Rust",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Embedded Systems",
+        "License :: OSI Approved :: MIT License"
+    ],
     entry_points={
         'console_scripts': [
             "oxidizer=src.main:entry"
         ]
     }
 )
+
