eb1fce02466706d8728abe02e09b8706f74ddce9
loxygenK <loxygen.k@gmail.com>
2020-11-29 17:12:52 +0900

    ✏️ Fixed syntax error

--- src/log.py
+++ src/log.py
@@ -6,7 +6,7 @@ class Logger:
         self.use_color = use_color
 
     def print_with_tag(tag_char: str, color: ConsoleBaseColor, text: str):
-        tag = (bold(color.paint(f"[{tag_char}]")))
+        tag = (bold(color.paint(f"[{tag_char}]"))) \
               if self.use_color else "[{tag_char}]"
         indented = "    ".join(text.split())
         main_text = color.paint(indented) if self.use_color else indented
@@ -15,7 +15,7 @@ class Logger:
     def info(text: str):
         print_with_tag("i", ConsoleBaseColor.CYAN, text)
 
-    def error(text: str)
+    def error(text: str):
         print_with_tag("!", ConsoleBaseColor.RED, text)
 
     def success(text: str):
