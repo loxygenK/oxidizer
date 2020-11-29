f17eb643d4a98069eefb4a4659ee440764375e11
flisan <loxygen.k@gmail.com>
2020-11-29 23:26:28 +0900

    🔖 Write documents and release 1.0.0 (#10)
    * 💥 Changed package name
    
    * 🔧 Added requirements.txt
    
    * 🔧 Make requirements.txt to be included in the package
    
    * 🚀 Write setup.py detail
    
    * 📝 Use README.md instead of rst document
    
    * 📝 Added "url" attribute
    
    * 📝 Write README.md with the best effort
    
    * 🔖 Patch version up to test README.md
    
    * 🔖 Re-test README.md
    
    * 🐛 Fix improper writing in MANIFEST.in
    
    * ⬇️ loosen the version specification
    
    * 🔖 Release 1.0.0
    
    * 🙈 Ignored build artifacts

--- setup.py
+++ setup.py
@@ -1,13 +1,43 @@
 from setuptools import setup
 
+
+def load_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def load_readme():
+    with open("README.md") as f:
+        return f.read()
+
+version = "1.0.0"
+
 setup(
-    name="oxidizer",
-    version="0.1.0",
+    name="arduino-oxidizer",
+    version=version,
+    url="https://github.com/loxygenK/oxidizer",
+    author="loxygen",
+    author_email="me@loxygen.dev",
+    maintainer="loxygen",
+    maintainer_email="me@loxygen.dev",
+    description="A python tool to build Rust project for Arduino and write it.",
+    long_description=load_readme(),
+    long_description_content_type="text/markdown",
     packages=["src"],
-    install_requires=["pyserial"],
+    install_requires=load_requirements(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Rust",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Embedded Systems",
+        "License :: OSI Approved :: MIT License"
+    ],
     entry_points={
         'console_scripts': [
             "oxidizer=src.main:entry"
         ]
     }
 )
+
